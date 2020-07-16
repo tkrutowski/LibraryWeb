5c482788c7719cdeda8d6d80426c49bce234a0bb
Tomasz Krutowski <tkrutowski@gmail.com>
2020-07-16 20:41:39 +0200

    add bookstore

--- src/main/java/DAO/BookstoreDao.java
+++ src/main/java/DAO/BookstoreDao.java
@@ -1,4 +1,118 @@
 package DAO;
 
+import Util.ConnectionFactory;
+import net.focik.Library.model.Bookstore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookstoreDao {
+    private static Logger logger = LoggerFactory.getLogger(BookstoreDao.class);
+
+    private final static String CREATE = "INSERT INTO bookstores(name, www) VALUES(?, ?);";
+    private final static String READ_ONE = "SELECT * FROM bookstores WHERE id_bookstore = ?;";
+    private final static String READ_ALL = "SELECT * FROM bookstores;";
+    private final static String UPDATE = "UPDATE bookstores SET name=?, www=? WHERE id_bookstore = ?;";
+    private final static String DELETE = "DELETE FROM bookstores WHERE id_bookstore=?;";
+
+    public int create(Bookstore bookstore)  {
+        int affectedRows=0;
+        try (Connection connection = ConnectionFactory.getConnection()){
+            PreparedStatement preparedStatement=connection.prepareStatement(CREATE);
+            preparedStatement.setString(1,bookstore.getName());
+            preparedStatement.setString(2,bookstore.getWww());
+
+            affectedRows=preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            logger.error("Can't add bookstore to db");
+        }
+        logger.info("Create new bookstore: {} {}",bookstore.getName(),bookstore.getWww());
+        return affectedRows;
+    }
+
+    public Bookstore readOne(int id)  {
+        Bookstore bookstore=null;
+        try (Connection connection=ConnectionFactory.getConnection()){
+            PreparedStatement preparedStatement=connection.prepareStatement(READ_ONE);
+            preparedStatement.setInt(1,id);
+
+            ResultSet resultSet=preparedStatement.executeQuery();
+
+            if(resultSet.next()) {
+                bookstore=new Bookstore();
+                bookstore.setIdBookStore(resultSet.getInt(1));
+                bookstore.setName(resultSet.getString(2));
+                bookstore.setWww(resultSet.getString(3));
+
+                logger.info("Readed bookstore: {} {} {}",bookstore.getIdBookStore(),bookstore.getName(),bookstore.getWww());
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            logger.error("Can't read bookstore from db");
+            return null;
+        }
+        return bookstore;
+    }
+
+    public List<Bookstore> readAll()  {
+        List<Bookstore> bookstores=new ArrayList<>();
+        try (Connection connection=ConnectionFactory.getConnection()){
+
+            Statement statement=connection.createStatement();
+
+            ResultSet resultSet=statement.executeQuery(READ_ALL);
+
+            while (resultSet.next()){
+                Bookstore bookstore = new Bookstore();
+                bookstore.setIdBookStore(resultSet.getInt(1));
+                bookstore.setName(resultSet.getString(2));
+                bookstore.setWww(resultSet.getString(3));
+
+                bookstores.add(bookstore);
+                logger.info("Added to list {} {} {}",bookstore.getIdBookStore(),bookstore.getName(),bookstore.getWww());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            logger.error("Can't read bookstores from db");
+            return null;
+        }
+        return bookstores;
+    }
+
+    public void update(Bookstore bookstore)  {
+        try (Connection conn=ConnectionFactory.getConnection();
+             PreparedStatement statement=conn.prepareStatement(UPDATE);){
+
+            statement.setString(1,bookstore.getName());
+            statement.setString(2,bookstore.getWww());
+            statement.setInt(3,bookstore.getIdBookStore());
+
+            statement.executeUpdate();
+            logger.info("Update bookstore {} {} {}",bookstore.getIdBookStore(),bookstore.getName(),bookstore.getWww());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            logger.error("Can't update bookstore in db");
+        }
+    }
+
+    public void delete(int id){
+        try (Connection connection = ConnectionFactory.getConnection()){
+
+            PreparedStatement statement=connection.prepareStatement(DELETE);
+            statement.setInt(1,id);
+            statement.executeUpdate();
+            logger.info("Deleted bookstore where id =  {}",id);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            logger.error("Can't delete bookstore in db");
+        }
+    }
 }
--- src/main/java/net/focik/Library/controller/BookstoreServlet.java
+++ src/main/java/net/focik/Library/controller/BookstoreServlet.java
@@ -1,10 +1,121 @@
-@javax.servlet.annotation.WebServlet(name = "BookstoreServlet")
-public class BookstoreServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+package net.focik.Library.controller;
+
+import DAO.AuthorDao;
+import DAO.BookstoreDao;
+import com.google.gson.Gson;
+import exceptions.NoEmptyValueException;
+import net.focik.Library.model.Author;
+import net.focik.Library.model.Bookstore;
+import net.focik.Library.model.OperationType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/BookstoreServlet")
+public class BookstoreServlet extends HttpServlet {
+    BookstoreDao bookstoreDao;
+    private static Logger logger = LoggerFactory.getLogger(BookstoreServlet.class);
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        OperationType operation = OperationType.valueOf(request.getParameter("operation").toUpperCase());
+
+        int idAuthor = Integer.parseInt(request.getParameter("id"));
+        Bookstore bookstore=(createBookstore(request, operation));
+        bookstoreDao = new BookstoreDao();
+
+        switch (operation) {
+            case DELETE:
+                bookstoreDao.delete(idAuthor);
+                break;
+            case UPDATE:
+                if(bookstore != null)
+                    bookstoreDao.update(createBookstore(request, operation));
+                else
+                    request.getRequestDispatcher("error.jsp").forward(request,response);
+                break;
+            case CREATE:
+                if(bookstore != null)
+                    bookstoreDao.create(createBookstore(request, operation));
+                else
+                    request.getRequestDispatcher("error.jsp").forward(request,response);
+                break;
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setCharacterEncoding("UTF-8");
+
+        List<Bookstore> bookstoreList = createBookstoreList(request);
+
+        Gson gson = new Gson();
+        String bookstoreJSON = gson.toJson(bookstoreList);
+
+        PrintWriter printWriter = response.getWriter();
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        printWriter.write(bookstoreJSON);
+        printWriter.close();
 
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+    private Bookstore createBookstore(HttpServletRequest request, OperationType operationType) {
+        Bookstore bookstore = null;
+        try {
+            int id = Integer.parseInt(request.getParameter("id"));
+            String name = request.getParameter("name");
+            String www = request.getParameter("www");
+            switch (operationType) {
+                case UPDATE:
+                    if (id == 0 || name == "") {
+                        throw new NoEmptyValueException();
+                    }
+                    break;
+                case CREATE:
+                    if (name == "") {
+                        throw new NoEmptyValueException();
+                    }
+                    break;
+            }
+            bookstore = new Bookstore();
+            bookstore.setIdBookStore(id);
+            bookstore.setName(name);
+            bookstore.setWww(www);
+        } catch (NoEmptyValueException e) {
+            e.printStackTrace();
+            logger.error("Can't create bookstore with empty parameters");
+        }
+        return bookstore;
+    }
 
+    private List<Bookstore> createBookstoreList(HttpServletRequest request) {
+
+        OperationType operation = OperationType.valueOf(request.getParameter("operation").toUpperCase());
+        int id = Integer.parseInt(request.getParameter("id"));
+        bookstoreDao = new BookstoreDao();
+        List<Bookstore> bookstoreList = new ArrayList<>();
+
+        switch (operation) {
+            case READ_ONE:
+                Bookstore bookstore = bookstoreDao.readOne(id);
+                bookstoreList.add(bookstore);
+                break;
+            case READ_ALL:
+                bookstoreList = bookstoreDao.readAll();
+                break;
+            case DELETE:
+                bookstoreDao.delete(id);
+                break;
+        }
+        return bookstoreList;
     }
 }
